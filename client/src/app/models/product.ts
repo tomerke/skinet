1110ec37f323f5bba29e356978af82749cc025e0
tomerke <tomersadi@gmail.com>
2021-01-08 12:29:11 +0200

    End of section8

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    pictureUrl: string;
+    procutType: string;
+    productBrand: string;
+}
+
